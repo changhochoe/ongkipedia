3b79a73b72bb762feffee185902a30a044e8b726
Jjangmo <cchoe3142@gmail.com>
2024-11-28 22:01:10 +1000

    js 코드 수정

--- make_new_document.js
+++ make_new_document.js
@@ -1,10 +1,17 @@
 document.getElementById('freakin_button').addEventListener('click',() => {
-    const ind_num = document.getElementById('index_nums').value;
-    console.log(ind_num)
-    if(ind_num<=0){
+    const indexNum = document.getElementById('index_nums').value;
+    console.log(indexNum)
+    if(indexNum<=0){
         alert("목차 수는 1 이상이어야 합니다");
         location.reload();
     } else {
         document.getElementById('hidden_content').style.display = 'flex';
+        for (let i = 1; i <= indexNum ; i++) {
+            let parent = document.getElementById('freakinSelect');
+            const Option = document.createElement('option');
+            Option.textContent = `목차 ${i}`
+            Option.value = i;
+            parent.appendChild(Option)
+        }
     }
 });
