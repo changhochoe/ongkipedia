cbbeb0da8c1b618117b109d138bd25188e7f8f97
Jjangmo <cchoe3142@gmail.com>
2024-11-29 00:15:33 +1000

    js 코드 수정 및 변경사항 감지기능 추가

--- make_new_document.js
+++ make_new_document.js
@@ -15,3 +15,15 @@ document.getElementById('freakin_button').addEventListener('click',() => {
         }
     }
 });
+
+document.getElementById('freakinSelect').addEventListener('change',()=>{
+    let detectTitleChange = document.getElementById('mokCha').value;
+    let detectContentChange = document.getElementById('naeYong').value;
+    if(detectContentChange.length >= 1 || detectTitleChange.length >= 1){
+        const unsavedChange = confirm('변경사항이 저장되지 않았습니다. 그래도 변경하시겠습니까?')
+        if(unsavedChange == true){
+            let clearInput = document.getElementById('naeYong');
+        }
+    }
+
+})
