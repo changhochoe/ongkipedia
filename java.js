4d5a987c4e856a54267c5ecd66b1a0babbf11cc0
Jjangmo <cchoe3142@gmail.com>
2024-11-29 18:25:06 +1000

    실검창 편집

--- java.js
+++ java.js
@@ -1,1 +1,6 @@
-
+document.getElementById("bruh").addEventListener('mouseenter', () => {
+    document.getElementById('shoNow').style.display ='block';
+})
+document.getElementById("shoNow").addEventListener('mouseleave', () => {
+    document.getElementById('shoNow').style.display ='none';
+})
