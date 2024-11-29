bad7809908ea163c2a5aa462d88e0f0963281fa1
Jjangmo <cchoe3142@gmail.com>
2024-11-29 13:56:18 +1000

    새문서 개설 파트 수정 최종본

--- make_new_document.js
+++ make_new_document.js
@@ -13,17 +13,57 @@ document.getElementById('freakin_button').addEventListener('click',() => {
             Option.value = i;
             parent.appendChild(Option)
         }
+            document.getElementById('selectOut').style.display = 'none';
     }
 });
 
-document.getElementById('freakinSelect').addEventListener('change',()=>{
-    let detectTitleChange = document.getElementById('mokCha').value;
-    let detectContentChange = document.getElementById('naeYong').value;
-    if(detectContentChange.length >= 1 || detectTitleChange.length >= 1){
-        const unsavedChange = confirm('변경사항이 저장되지 않았습니다. 그래도 변경하시겠습니까?')
-        if(unsavedChange == true){
-            let clearInput = document.getElementById('naeYong');
-        }
-    }
+function deleteInput(id){
+    document.getElementById(id).value=''
+}
 
+
+
+
+let detectTitleChange = document.getElementById('mokCha').value;
+let detectContentChange = document.getElementById('naeYong').value;
+let detectSelectOption = document.getElementById('freakinSelect').value;
+
+
+document.getElementById('mokCha').addEventListener('click',() => {
+    detectSelectOption = document.getElementById('freakinSelect').value;
+    if(detectSelectOption[3] == '순'){
+        alert('목차 순서를 먼저 골라주세요');
+        location.reload();
+    }
+})
+
+document.getElementById('naeYong').addEventListener('click',() => {
+    detectSelectOption = document.getElementById('freakinSelect').value;
+    if(detectSelectOption[3] == '순'){
+        alert('목차 순서를 먼저 골라주세요');
+        location.reload();
+    }
+})
+
+
+
+document.getElementById('freakinSelect').addEventListener('click',()=>{
+    detectSelectOption = document.getElementById('freakinSelect').value;
+    function originalOption(id){
+        document.getElementById(id).value = detectSelectOption
+    }
+    document.getElementById('freakinSelect').addEventListener('change',()=>{
+        detectTitleChange = document.getElementById('mokCha').value;
+        detectContentChange = document.getElementById('naeYong').value;
+        if(detectContentChange.length >= 1 || detectTitleChange.length >= 1){
+            const unsavedChange = confirm('변경사항이 저장되지 않았습니다. 그래도 변경하시겠습니까?')
+            if(unsavedChange == true){
+                deleteInput('mokCha');
+                deleteInput('naeYong');
+            } else{
+                console.log(detectSelectOption)
+                originalOption('freakinSelect')
+            }
+        }
+    })    
 })
