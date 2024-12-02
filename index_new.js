254c2a47bf9800de83bf14ad4e189ba41ec4f3b0
Jjangmo <cchoe3142@gmail.com>
2024-12-03 01:45:58 +1000

    메인화면 추가 및 문서명 변경

---
+++ index_new.js
@@ -0,0 +1,27 @@
+// document.getElementById('background').addEventListener('mouseenter',()=>{
+//     document.getElementById('background').style.backgroundColor = 'yellow';
+//     document.getElementById('hole1').style.backgroundColor = 'yellow';
+//     document.getElementById('hole2').style.backgroundColor = 'yellow';
+//     document.getElementById('hole3').style.backgroundColor = 'yellow';
+//     document.getElementById('hole4').style.backgroundColor = 'yellow';
+//     document.getElementById('hole5').style.backgroundColor = 'yellow';
+// })
+
+
+document.getElementById('background').style.display = 'flex'
+
+document.getElementById('background').addEventListener('mouseenter',()=>{
+    document.getElementById('greet').style.visibility = 'visible';
+    document.getElementById('greet').style.opacity = 1;
+    let idList =['background','hole1','hole2','hole3','hole4','hole5']
+    for(i of idList){
+        document.getElementById(i).style.backgroundColor = 'yellow'
+    }
+})
+
+document.getElementById('background').addEventListener('mouseleave',()=>{
+    let idList =['background','hole1','hole2','hole3','hole4','hole5']
+    for(i of idList){
+        document.getElementById(i).style.backgroundColor = 'white'
+    }
+})
